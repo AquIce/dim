b5c87452c936cdb52035a7c2aad5df4208229894
AquIce <aquice.dev@gmail.com>
2025-09-13 16:33:34 +0200

    Added structs declaration to interpreter

--- src/runner/interpreter/value.cxx
+++ src/runner/interpreter/value.cxx
@@ -10,8 +10,8 @@ namespace dim {
 		std::string Value::Repr() {
 			return "INVALID";
 		}
-		ValueType Value::Type() {
-			return ValueType::NONE;
+		parser::DatatypeStr Value::Type() {
+			return "INFER";
 		}
 
 		ValueFlagWVal Value::GetFlag() {
@@ -142,8 +142,8 @@ namespace dim {
 		std::string NullValue::Repr() {
 			return "null";
 		}
-		ValueType NullValue::Type() {
-			return ValueType::NUL;
+		parser::DatatypeStr NullValue::Type() {
+			return "VOID";
 		}
 
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(NullValue, +, null)
@@ -164,19 +164,19 @@ namespace dim {
 		__GEN__UNARY_OPERATOR_VALUE_BODY_OVERRRIDE_ERR(NullValue, ~, null)
 
 
-		__GEN__NUMBER_IU_CLASS_IMPL(I8Value, i8, I8)
-		__GEN__NUMBER_IU_CLASS_IMPL(I16Value, i16, I16)
-		__GEN__NUMBER_IU_CLASS_IMPL(I32Value, i32, I32)
-		__GEN__NUMBER_IU_CLASS_IMPL(I64Value, i64, I64)
+		__GEN__NUMBER_IU_CLASS_IMPL(I8Value, i8, "I8")
+		__GEN__NUMBER_IU_CLASS_IMPL(I16Value, i16, "I16")
+		__GEN__NUMBER_IU_CLASS_IMPL(I32Value, i32, "I32")
+		__GEN__NUMBER_IU_CLASS_IMPL(I64Value, i64, "I64")
 
-		__GEN__NUMBER_IU_CLASS_IMPL(U8Value, u8, U8)
-		__GEN__NUMBER_IU_CLASS_IMPL(U16Value, u16, U16)
-		__GEN__NUMBER_IU_CLASS_IMPL(U32Value, u32, U32)
-		__GEN__NUMBER_IU_CLASS_IMPL(U64Value, u64, U64)
+		__GEN__NUMBER_IU_CLASS_IMPL(U8Value, u8, "U8")
+		__GEN__NUMBER_IU_CLASS_IMPL(U16Value, u16, "U16")
+		__GEN__NUMBER_IU_CLASS_IMPL(U32Value, u32, "U32")
+		__GEN__NUMBER_IU_CLASS_IMPL(U64Value, u64, "U64")
 
-		__GEN__NUMBER_F_CLASS_IMPL(F32Value, f32, F32, std::to_string)
-		__GEN__NUMBER_F_CLASS_IMPL(F64Value, f64, F64, std::to_string)
-		__GEN__NUMBER_F_CLASS_IMPL(F128Value, f128, F128, utils::f128tos)
+		__GEN__NUMBER_F_CLASS_IMPL(F32Value, f32, "F32", std::to_string)
+		__GEN__NUMBER_F_CLASS_IMPL(F64Value, f64, "F64", std::to_string)
+		__GEN__NUMBER_F_CLASS_IMPL(F128Value, f128, "F128", utils::f128tos)
 
 
 		BooleanValue::BooleanValue(
@@ -202,8 +202,8 @@ namespace dim {
 		std::string BooleanValue::Repr() {
 			return m_value ? "true" : "false";
 		}
-		ValueType BooleanValue::Type() {
-			return ValueType::BOOLEAN;
+		parser::DatatypeStr BooleanValue::Type() {
+			return "BOOLEAN";
 		}
 
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(BooleanValue, +, boolean)
@@ -215,10 +215,10 @@ namespace dim {
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(BooleanValue, <=, boolean)
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(BooleanValue, >=, boolean)
 
-		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(BooleanValue, BooleanValue, ValueType::BOOLEAN, &&)
-		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(BooleanValue, BooleanValue, ValueType::BOOLEAN, ||)
-		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(BooleanValue, BooleanValue, ValueType::BOOLEAN, ==)
-		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(BooleanValue, BooleanValue, ValueType::BOOLEAN, !=)
+		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(BooleanValue, BooleanValue, "BOOLEAN", &&)
+		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(BooleanValue, BooleanValue, "BOOLEAN", ||)
+		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(BooleanValue, BooleanValue, "BOOLEAN", ==)
+		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(BooleanValue, BooleanValue, "BOOLEAN", !=)
 		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_ADVANCED_BITWISE(BooleanValue, &)
 		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_ADVANCED_BITWISE(BooleanValue, |)
 		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_ADVANCED_BITWISE(BooleanValue, ^)
@@ -254,8 +254,8 @@ namespace dim {
 		std::string CharValue::Repr() {
 			return "'" + std::string(1, m_value) + "'";
 		}
-		ValueType CharValue::Type() {
-			return ValueType::CHAR;
+		parser::DatatypeStr CharValue::Type() {
+			return "CHAR";
 		}
 
 		std::expected<
@@ -264,21 +264,20 @@ namespace dim {
 		> CharValue::operator+(
 			std::shared_ptr<Value> other
 		) {
-			switch(other->Type()) {
-			case ValueType::CHAR:
+			if(other->Type() == "CHAR") {
 				return std::make_shared<StringValue>(
 					std::string(1, this->GetValue()) + std::dynamic_pointer_cast<CharValue>(other)->GetValue()
 				);
-			case ValueType::STRING:
+			}
+			if(other->Type() == "STRING") {
 				return std::make_shared<StringValue>(
 					std::string(1, this->GetValue()) + std::dynamic_pointer_cast<StringValue>(other)->GetValue()
 				);
-			default:
-				return std::unexpected(
-					std::string("Cannot use '+' operator on CharValue value and ")
-					+ std::string(ValueTypeStr.at(int(other->Type())))
-				);
 			}
+			return std::unexpected(
+				std::string("Cannot use '+' operator on CharValue value and ")
+				+ other->Type()
+			);
 		}
 		
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(CharValue, -, char)
@@ -291,8 +290,8 @@ namespace dim {
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(CharValue, &&, char) 
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(CharValue, ||, char)
 
-		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(CharValue, BooleanValue, ValueType::CHAR, ==)
-		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(CharValue, BooleanValue, ValueType::CHAR, !=)
+		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(CharValue, BooleanValue, "CHAR", ==)
+		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(CharValue, BooleanValue, "CHAR", !=)
 
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(CharValue, &, char)
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(CharValue, |, char) 
@@ -323,8 +322,8 @@ namespace dim {
 		std::string StringValue::Repr() {
 			return "\"" + m_value + "\"";
 		}
-		ValueType StringValue::Type() {
-			return ValueType::STRING;
+		parser::DatatypeStr StringValue::Type() {
+			return "STRING";
 		}
 
 		std::expected<
@@ -333,21 +332,20 @@ namespace dim {
 		> StringValue::operator+(
 			std::shared_ptr<Value> other
 		) {
-			switch(other->Type()) {
-			case ValueType::STRING:
+			if(other->Type() == "STRING") {
 				return std::make_shared<StringValue>(
 					this->GetValue() + std::dynamic_pointer_cast<StringValue>(other)->GetValue()
 				);
-			case ValueType::CHAR:
+			}
+			if(other->Type() == "CHAR") {
 				return std::make_shared<StringValue>(
 					this->GetValue() + std::string(1, std::dynamic_pointer_cast<CharValue>(other)->GetValue())
 				);
-			default:
-				return std::unexpected(
-					std::string("Cannot use '+' operator on StringValue value and ")
-					+ std::string(ValueTypeStr.at(int(other->Type())))
-				);
 			}
+			return std::unexpected(
+				std::string("Cannot use '+' operator on StringValue value and ")
+				+ other->Type()
+			);
 		}
 		
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(StringValue, -, string)
@@ -360,8 +358,8 @@ namespace dim {
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(StringValue, &&, string) 
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(StringValue, ||, string)
 
-		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(StringValue, BooleanValue, ValueType::STRING, ==)
-		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(StringValue, BooleanValue, ValueType::STRING, !=)
+		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(StringValue, BooleanValue, "STRING", ==)
+		__GEN__OPERATOR_VALUE_BODY_OVERRIDE_WDISCARD(StringValue, BooleanValue, "STRING", !=)
 
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(StringValue, &, string)
 		__GEN__OPERATOR_VALUE_BODY_OVERRRIDE_ERR(StringValue, |, string) 
